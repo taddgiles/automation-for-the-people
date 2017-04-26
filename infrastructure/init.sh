c6be80bba38b2c6ad1547ed3e022ec42ddd921f4
Tadd Giles <tadd@taddgiles.com>
2017-04-26 16:32:26 -0600

    update to core

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-echo "enter your GitHub user name. The user must own the fork!"
-read GITHUB_USER_NAME
-
-echo "enter a personal GitHub access token. Generate one here: https://github.com/settings/tokens (scopes needed: admin:repo_hook, repo)"
+echo "Enter a GitHub access token for the KualiCoSaaSOps account. (scopes needed: admin:repo_hook, repo)"
 read GITHUB_OAUTH_TOKEN
 
-aws cloudformation create-stack \\
-  --region eu-west-1 \\
-  --stack-name "aftp-pipeline"
-  --template-body file://pipeline.yaml
-  --capabilities CAPABILITY_IAM
-  --parameters \\
-    ParameterKey=GitHubUserName,ParameterValue=$GITHUB_USER_NAME \\
+echo "Enter the name of the GitHub repo to deploy"
+read $GITHUB_REPO
+
+aws cloudformation create-stack \
+  --region us-west-2 \
+  --stack-name $GITHUB_REPO-us-west-2-cp \
+  --template-body file://pipeline.yaml \
+  --capabilities CAPABILITY_IAM \
+  --parameters \
     ParameterKey=GitHubOAuthToken,ParameterValue=$GITHUB_OAUTH_TOKEN
+    ParameterKey=GitHubRepo,ParameterValue=$GITHUB_REPO
